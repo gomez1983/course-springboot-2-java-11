b6598b76ce985ca203fa746de3a1d7def32679d6
Andre Gomez <andregc1983@gmail.com>
2020-11-07 20:09:50 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,11 +1,16 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 //****Essa classe é um recurso web implementado pelo controlador Web
 
@@ -13,9 +18,20 @@ import com.educandoweb.course.entities.User;
 @RequestMapping(value = "/users")
 public class UserResource {
 
+// Dependência para o service
+	@Autowired
+	private UserService service; // Para isso funcionar, a classe tem que estar registrada como um componente do Spring
+	
 	@GetMapping // Anotação para indicar que é uma requisição do tipo get do HTTP
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-		return ResponseEntity.ok().body(u); // Retorna no HTTP. O "body" faz retornar a resposta
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list); // Retorna no HTTP. O "body" faz retornar a resposta
+	}
+	
+	//Implementação para buscar um usuário por ID
+	@GetMapping(value = "/{id}") // Passa na URL o valor da ID do usuário. Indica que a requisição aceita um ID dentro da URL
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj); //"Ok" indica que teve sucesso. Corpo da requisição "body", vai o "obj".
 	}
 }
---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Service // Anotação que registra a classe como um serviço (componente) do Spring
+public class UserService { // Implementação para buscar todos os usuários e buscá-los por ID.
+	
+	@Autowired
+	private UserRepository repository;
+	
+	//Método para retornar todos os usuários do banco de dados
+	public List<User> findAll (){
+		return repository.findAll(); // Repassa a chamada para "repository.findAll"
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get(); // Método retorna o objeto do tipo "User" que estiver dentro do "Optional"
+	}
+}
