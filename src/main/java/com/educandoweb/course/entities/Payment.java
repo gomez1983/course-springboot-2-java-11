0075c65a24d032a95965267f94ed968f1218c711
Andre Gomez <andregc1983@gmail.com>
2020-11-24 21:17:07 -0300

    Payment, one-to-one association

---
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -0,0 +1,87 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable { // Quando você quer que os objetos sejam transformados em cadeia de bystes, para que possam trafegar na rede, ou gravados em arquivos...
+
+	private static final long serialVersionUID = 1L; // Número de série padrão do Serializable
+
+	@Id //Indica qual atributo vai ter a chave primária da tabela do banco de dados.
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //Identifica que ela é autoincrementável no BD
+	private Long id;
+	private Instant moment;
+	
+	@OneToOne // Indica que essa classe é dependente
+	@MapsId
+	private Order order;
+
+	public Payment() {
+	}
+
+	public Payment(Long id, Instant moment, Order order) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.order = order;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
